72374c740e68d85a71824688502c0956e0151e60
ad.osipov <ad.osipov@tensor.ru>
2022-02-11 13:44:34 +0500

    Задание по объектам_

--- src/Objects.js
+++ src/Objects.js
@@ -1,12 +1,3 @@
-/*
-  В функцию personUpdate() приходят данные в виде объекта, содержащую некую информацию о человеке.
-  Если этот человек является женщиной (свойство gender содержит значение 'female'), то из этого объекта
-  необходимо удалить свойство age, если оно есть.
-  Если этот человек является мужчиной (свойство gender содержит значение 'male'), следует убедиться,
-  что в этом объекте есть свойство income. Если его нет, необходимо его добавить
-  и присвоить начальное значение 100000.
-  Объект после манипуляций следует вернуть в качестве результата работы функции.
-*/
 export function personUpdate(data) {
     let result = { ...data };
     if (result.gender === 'female' && result.hasOwnProperty('age')) {
@@ -18,10 +9,6 @@ export function personUpdate(data) {
     return result;
 }
 
-/*
-  В функцию objectFieldsList приходят три объекта с различными полями, список которых заранее неизвестен.
-  Верните список названий этих полей в алфавитном порядке в виде массива строк.
-*/
 export function objectFieldsList(obj1, obj2, obj3) {
     let total = Object.keys(obj1);
     total = total.concat(Object.keys(obj2));
@@ -29,11 +16,6 @@ export function objectFieldsList(obj1, obj2, obj3) {
     return total.sort();
 }
 
-/*
-  Верните в результате работы функции массив с клонами объекта obj.
-  При этом каждый клон должен дополнительно содержать поле id со своим порядковым номером в массиве.
-  Количество клонов - count.
-*/
 export function objectClone(obj, count) {
     let result = [];
     for (let i = 0; i < count; i++) {
